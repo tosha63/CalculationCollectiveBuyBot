2148264206c27f867546edf46f75ddedaf79e34c
Anton Shtanko <shtanko-a-o@mail.ru>
2022-08-10 14:12:57 +0400

    added stub telegram bot to the project

---
+++ src/main/java/com/github/shtanko/tgbot/calculation_collective_buy_bot/bot/CalculationTelegramBot.java
@@ -0,0 +1,50 @@
+package com.github.shtanko.tgbot.calculation_collective_buy_bot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@Component
+public class CalculationTelegramBot extends TelegramLongPollingBot {
+
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+
+            SendMessage sm = new SendMessage();
+            sm.setText(message);
+            sm.setChatId(chatId);
+
+
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
