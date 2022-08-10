1a723ee0284495e50ce7f85b996159b0c2db5da7
Anton Shtanko <shtanko-a-o@mail.ru>
2022-08-10 10:57:56 +0400

    added SpringBoot skeleton to the project

---
+++ src/main/java/com/github/shtanko/tgbot/calculation_collective_buy_bot/CalculationCollectiveBuyBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.shtanko.tgbot.calculation_collective_buy_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationCollectiveBuyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationCollectiveBuyBotApplication.class, args);
+	}
+
+}
